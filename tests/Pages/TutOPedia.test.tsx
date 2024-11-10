95804e96f29f91cccac91b4db7167a1bdbb269a8
rwdevops999 <rwdevops999@gmail.com>
2024-11-10 16:03:37 +0100

    Update TutOPedia.test.tsx
    all tests passed (5/5)

--- tests/Pages/TutOPedia.test.tsx
+++ tests/Pages/TutOPedia.test.tsx
@@ -1,35 +1,37 @@
 import {
   ROUTE_TUTOPEDIA,
+  ROUTE_TUTORIALS,
   TUTOPEDIA_CONTENT,
+  TUTOPEDIA_CONTENT_SERVER_CHECK_PAGE,
   TUTOPEDIA_FOOTER,
   TUTOPEDIA_HEADER,
 } from "../../src/data/layout/layout";
 import { renderRoute } from "../render/render";
 import { expectInDocumentByTestId } from "../testutils";
 
-describe.skip("TutOPedia", () => {
+describe("TutOPedia", () => {
   beforeEach(async () => {
-    renderRoute(ROUTE_TUTOPEDIA);
+    renderRoute(`${ROUTE_TUTOPEDIA}`);
   });
 
   it("should contain the header", () => {
-    expectInDocumentByTestId(TUTOPEDIA_HEADER);
+    expectInDocumentByTestId(`${TUTOPEDIA_HEADER}`);
   });
 
   it("should contain the content", () => {
-    expectInDocumentByTestId(TUTOPEDIA_CONTENT);
+    expectInDocumentByTestId(`${TUTOPEDIA_CONTENT}`);
   });
 
-  it.skip("should contain the ServerCheckPage", () => {
-    expectInDocumentByTestId("SERVERCHECK_PAGE");
+  it("should contain the ServerCheckPage", () => {
+    expectInDocumentByTestId(`${TUTOPEDIA_CONTENT_SERVER_CHECK_PAGE}`);
   });
 
-  it.skip("should contain the TutorialsPage", () => {
-    renderRoute("/tutorials");
-    expectInDocumentByTestId("SERVERCHECK_PAGE");
+  it("should contain the TutorialsPage", () => {
+    renderRoute(`/${ROUTE_TUTORIALS}`);
+    expectInDocumentByTestId(`${TUTOPEDIA_CONTENT_SERVER_CHECK_PAGE}`);
   });
 
   it("should contain the footer", () => {
-    expectInDocumentByTestId(TUTOPEDIA_FOOTER);
+    expectInDocumentByTestId(`${TUTOPEDIA_FOOTER}`);
   });
 });
