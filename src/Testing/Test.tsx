7d47b56e35bab4224bc73466bfd9d67a6cd9fa12
rwdevops999 <rwdevops999@gmail.com>
2024-11-10 15:33:03 +0100

    reset
    Test.tsx and .css

--- src/Testing/Test.tsx
+++ src/Testing/Test.tsx
@@ -2,31 +2,7 @@ import { Box } from "@mui/material";
 import "./Test.css";
 
 const Test = () => {
-  return (
-    <Box id="XXX" width={"100%"} height={"20%"}>
-      <div className="radio-inputs">
-        <label>
-          <input className="radio-input" type="radio" name="engine" />
-          <span className="radio-tile">
-            <span className="radio-icon">
-              <img className="icon-26" src={`/src/assets/admingear.svg`} />
-            </span>
-            <span className="radio-label">Settings</span>
-          </span>
-        </label>
-
-        <label>
-          <input checked className="radio-input" type="radio" name="engine" />
-          <span className="radio-tile">
-            <span className="radio-icon">
-              <img className="icon-26" src={`/src/assets/adminbucket.svg`} />
-            </span>
-            <span className="radio-label">Buckets</span>
-          </span>
-        </label>
-      </div>
-    </Box>
-  );
+  return <Box id="XXX" width={"100%"} height={"20%"}></Box>;
 };
 
 export default Test;
