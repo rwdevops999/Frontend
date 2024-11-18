ac913d9cc8e9c2c2b476ecf64b177c26662f45f8
rwdevops999 <rwdevops999@gmail.com>
2024-11-18 12:40:36 +0100

    provided
    2 images (with random display)

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -1,10 +1,18 @@
 import { Box } from "@mui/material";
 import { TUTOPEDIA_CONTENT_IMAGE_PAGE } from "../data/layout/layout";
 
+const getRandomInt = (max: number) => {
+  return Math.round(Math.random() * max) + 1;
+};
+
 const IntroPage = () => {
+  const getImageSrc = () => {
+    return "/src/assets/tutopedia" + getRandomInt(1) + ".jpeg";
+  };
+
   return (
     <Box data-title={TUTOPEDIA_CONTENT_IMAGE_PAGE} sx={{ marginLeft: "24%" }}>
-      <img src="/src/assets/tutopedia.jpeg" width="800" height="735" />
+      <img src={getImageSrc()} width="800" height="735" />
     </Box>
   );
 };
